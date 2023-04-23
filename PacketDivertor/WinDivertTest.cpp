c44d6a7dd93401644484f98559ed57b30f41ae75
omer <omercohen280105@gmail.com>
2023-04-23 09:07:30 +0300

    added packet divertor

--- PacketDivertor/WinDivertTest.cpp
+++ PacketDivertor/WinDivertTest.cpp
@@ -1,26 +1,96 @@
 #include <winsock2.h>
 #include <windows.h>
+#pragma comment(lib,"ws2_32")
+#pragma warning(disable : 4996)
 #include <psapi.h>
 #include <shlwapi.h>
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 
 #include "windivert.h"
 
+#define _my_addr_ "192.168.0.120"
+
 #define INET6_ADDRSTRLEN    45
+SOCKET* sock;
+HANDLE mutex;
+
+SOCKET* SetPythonSocket() {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"WSAStartup function failed with error: %d\n", iResult);
+        return nullptr;
+    }
+
+    SOCKET* sock = new SOCKET{ socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) };
+    if (*sock == INVALID_SOCKET) {
+        wprintf(L"socket function failed with error = %d\n", WSAGetLastError());
+        return nullptr;
+    }
+    sockaddr_in saServer;
+    saServer.sin_family = AF_INET;
+    saServer.sin_addr.S_un.S_addr = inet_addr(_my_addr_);
+    //InetPton(AF_INET, (PCWSTR)_my_addr_, &saServer.sin_addr.S_un.S_addr);
+    saServer.sin_port = htons(50123);
+    iResult = connect(*sock, (SOCKADDR*)&saServer, sizeof(saServer));
+    if (iResult == SOCKET_ERROR) {
+        std::cout << "error connecting to server\n";
+        return nullptr;
+    }
+    return sock;
+}
+
+void updatePythonProgram(UINT16 port,BOOL remove) {
+    DWORD result = WaitForSingleObject(mutex, INFINITE);
+    if (result == WAIT_OBJECT_0) {
+        // Convert value to network byte order
+        UINT16 networkValue = htons(port);
+
+        // Copy networkValue into buffer
+        char buffer[sizeof(UINT16)+sizeof(char)];
+        memcpy(buffer, (remove?"x":"y"), sizeof(char));
+        memcpy(buffer+sizeof(char), &networkValue, sizeof(UINT16));
+        send(*sock, buffer, sizeof(UINT16) + sizeof(char), 0);
+        ReleaseMutex(mutex);
+    } else {
+        // Failed to wait for mutex
+        std::cout << "waiting for mutex failed\n";
+        exit(1);
+    }
+}
 
 int main()
 {
+    SECURITY_ATTRIBUTES securityAttributes;
+    securityAttributes.nLength = sizeof(SECURITY_ATTRIBUTES);
+    securityAttributes.lpSecurityDescriptor = NULL;
+    securityAttributes.bInheritHandle = FALSE;
+    mutex = CreateMutexA(&securityAttributes, FALSE, "portMutex11");
+    if (mutex == NULL) {
+        // Failed to open mutex
+        std::cout << "opening mutex failed\n";
+        return 1;
+    }
+    ReleaseMutex(mutex);
+    std::cout << "released mutex\n";
+    sock = SetPythonSocket();
+    if (sock == nullptr || *sock == INVALID_SOCKET || *sock == NULL) {
+        std::cout << "error connecting to python server\n";
+        TerminateProcess(GetCurrentProcess(), 1);
+    }
+
     HANDLE handle, process;
     INT16 priority = 1121;          // Arbitrary.
-    const char* filter = "processId == 13680", * err_str;
+    const char* filter = "processId == 8452", * err_str;
     char path[MAX_PATH + 1];
     char local_str[INET6_ADDRSTRLEN + 1], remote_str[INET6_ADDRSTRLEN + 1];
     char* filename;
     DWORD path_len;
     WINDIVERT_ADDRESS addr;
 
-    handle = WinDivertOpen(filter, WINDIVERT_LAYER_SOCKET, priority, 0 | WINDIVERT_FLAG_RECV_ONLY);
+    handle = WinDivertOpen(filter, WINDIVERT_LAYER_SOCKET, priority, WINDIVERT_FLAG_SNIFF | WINDIVERT_FLAG_RECV_ONLY);
     if (handle == INVALID_HANDLE_VALUE)
     {
         if (GetLastError() == ERROR_INVALID_PARAMETER &&
@@ -42,9 +112,33 @@ int main()
             continue;
         }
 
+        std::cout << "got event\nport:" << addr.Socket.LocalPort << ' ';
+        switch (addr.Event)
+        {
+        case WINDIVERT_EVENT_SOCKET_BIND:
+            printf("BIND");
+            break;
+        case WINDIVERT_EVENT_SOCKET_LISTEN:
+            printf("LISTEN");
+            break;
+        case WINDIVERT_EVENT_SOCKET_CONNECT:
+            printf("CONNECT");
+            break;
+        case WINDIVERT_EVENT_SOCKET_ACCEPT:
+            printf("ACCEPT");
+            break;
+        case WINDIVERT_EVENT_SOCKET_CLOSE:
+            printf("CLOSE");
+            break;
+        default:
+            printf("???");
+            break;
+        }
 
-        WinDivertSend(handle, nullptr, 0, NULL, &addr)
+        std::cout << '\n';
+
+       // WinDivertSend(handle, nullptr, 0, NULL, &addr);
     }
-
+    CloseHandle(mutex);
     return 0;
 }
--- PacketDivertor/packet_diverter.py
+++ PacketDivertor/packet_diverter.py
@@ -1,22 +1,42 @@
 import pydivert
 import psutil
+import win32event
+import _thread
+from socket import *
+
+NOT_ALLOWED_PORTS = []
+
+GLOBAL_MUTEX = win32event.CreateMutex(None,False,"portMutex11")
+
+conn_sock = socket(AF_INET,SOCK_STREAM)
+conn_sock.bind(("192.168.0.120",50123))
+conn_sock.listen(1)
+sock,addr = conn_sock.accept()
+
+def update_not_allowed_ports():
+    data = sock.recv(1054).decode()
+    sign,port = data[0],data[1:]
+    if(sign == "x"):
+        try:
+            NOT_ALLOWED_PORTS.remove(port)
+        except ValueError:
+            pass
+    else:
+        if(NOT_ALLOWED_PORTS.count(port) == 0):
+            NOT_ALLOWED_PORTS.append(port)
+
+
+
+# def find_all_open_ports_of_process_id(pid:int)->list[int]:
+#     result = []
+#     for conn in psutil.net_connections():
+#         if conn.pid == pid:
+#             result.append(conn.laddr.port)
+#     return result
+
 
 '''
-infinte loop over pydivert handle and stop all outgoing/incoming packets to said ports
-
-possible solution - 
-    for each packet find its process ID, than decide if it can go through or not.
-'''
-
-
-def find_all_open_ports_of_process_id(pid:int)->list[int]:
-    result = []
-    for conn in psutil.net_connections():
-        if conn.pid == pid:
-            result.append(conn.laddr.port)
-    return result
-
-def find_pid_of_port(port: int, is_outbound: bool) -> int:
+def is_port_allowed(port: int, is_outbound: bool) -> int:
     if port is None:
         return None
     for conn in psutil.net_connections():
@@ -27,22 +47,29 @@ def find_pid_of_port(port: int, is_outbound: bool) -> int:
             if conn.raddr and conn.raddr.port == port:
                 return conn.pid
     return None
+'''
 
-# SPECIAL_PID = input("what pid to block? ")
-process = psutil.process_iter(["pid","name"])
-for proc in process:
-    if(proc.pid == 12168):
-        print(proc.connections())
+def is_port_allowed(port: int) -> bool:
+    if port is None:
+        return None
+    result = win32event.WaitForSingleObject(GLOBAL_MUTEX, 0)
+    if result == win32event.WAIT_OBJECT_0:
+        win32event.ReleaseMutex(GLOBAL_MUTEX)
+        if(NOT_ALLOWED_PORTS.count(port) == 0):
+            return True
+        return False
+    elif result == win32event.WAIT_TIMEOUT:
+        update_not_allowed_ports()
+        return is_port_allowed(port)
+    
 
 
-
-exit(1)
 while True:
     with pydivert.WinDivert("outbound or inbound") as w:
         for packet in w:
             if packet.is_inbound:
-                print(f"dst_port: {packet.dst_port}, src_port: {packet.src_port}, pid: {find_pid_of_port(packet.dst_port,packet.is_outbound)}, INBOUND")
-                if find_pid_of_port(packet.dst_port,packet.is_outbound) == SPECIAL_PID:
+                #print(f"dst_port: {packet.dst_port}, src_port: {packet.src_port}, pid: {is_port_allowed(packet.dst_port,packet.is_outbound)}, INBOUND")
+                if not is_port_allowed(packet.dst_port):
                     print(packet.payload)
                     x = input("walla")
                     '''
@@ -50,8 +77,8 @@ while True:
                     '''
                     continue
             else:
-                print(f"dst_port: {packet.dst_port}, src_port: {packet.src_port}, pid: {find_pid_of_port(packet.src_port,packet.is_outbound)}, OUTBOUND")
-                if find_pid_of_port(packet.src_port,packet.is_outbound) == SPECIAL_PID:
+                #print(f"dst_port: {packet.dst_port}, src_port: {packet.src_port}, pid: {is_port_allowed(packet.src_port,packet.is_outbound)}, OUTBOUND")
+                if not is_port_allowed(packet.src_port):
                     print(packet.payload)
                     x = input("walla")
                     '''
